83f3a19ea92adcbb033ac2d29940914c64b1d539
mohamed-sameh-albaz <136837275+mohamed-sameh-albaz@users.noreply.github.com>
2025-01-25 20:10:19 +0200

    feat: adding user/tour CRUDs routes

---
+++ routes/tourRouter.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import tourController from './../controllers/tourController'
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+export default router;
