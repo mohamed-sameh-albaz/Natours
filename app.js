ec6a34116a4970c3667940898d86c3ba0c8f7937
mohamed-sameh-albaz <mohamedalbaz492@gmail.com>
2025-04-11 14:58:47 +0200

    adding security stuff to the app

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,12 +14,47 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 app.use(express.json());
 
-// Middlewares
+// Global Middlewares
+
+// Set security HTTP headers
+app.use(helmet());
+
+// dev logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+//Limit sent requests
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 100,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
 // app.use(express.static(`${__dirname}/public`))
 
 // Routes
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const dotenv = require('dotenv');
-const app = require('./app.js');
 const mongoose = require('mongoose');
+const app = require('./app.js');
 
 process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log('UNCAUGHT EXCEPTION!💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
 dotenv.config({ path: './config.env' });
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
