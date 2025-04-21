b512d28ff9551f625bc4e0232eb764a5e4efb371
mohamed-sameh-albaz <mohamedalbaz492@gmail.com>
2025-04-21 09:55:44 +0200

    modify authentication and apply new controller handlers to routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,22 +8,29 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgetPassword', authController.forgetPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+//protect all routes after this middleware
+router.use(authController.protect);
+
 router.patch(
   '/updateMyPassword',
-  authController.protect,
   authController.updatePassword
 );
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.get('/me', userController.getMe, userController.getUser)
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
-router.route('/').get(userController.getAllUsers);
+//restrict all routes after this middleware to admin
+router.use(authController.restrictTo('admin'));
+
+router.route('/').get(userController.getAllUsers).post(userController.createUser);
 
 //   .post(userController.createUser);
-// router
-//   .route('/:id')
-//   .get(userController.getUser)
-//   .patch(userController.updateUser)
-//   .delete(userController.deleteUser);
+router
+  .route('/:id')
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser)
+  .get(userController.getUser);
 
 module.exports = router;
