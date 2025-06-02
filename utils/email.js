ce66c94376de3ee879dc6489d90c50117b13c829
mohamed-sameh-albaz <mohamedalbaz492@gmail.com>
2025-06-02 14:36:16 +0300

    add email configurations and views

---
+++ utils/email.js
@@ -0,0 +1,75 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstname = user.firstname;
+    this.from = `Natours <${process.env.EMAIL_FROM}>`;
+    this.url = url;
+    this.id = user.id;
+  }
+
+  newTransporter() {
+    if (process.env.NODE_ENV === 'production') {
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
+    console.log('Transport config:', {
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    });
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // send actual email
+  async send(template, subject) {
+    // 1) render a HTML based on the pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstname: this.firstname,
+        url: this.url,
+        subject,
+      }
+    );
+    // 2) define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      text: htmlToText.convert(html), // text version is better for email delivery rates and for spam folders
+      html,
+    };
+
+    console.log('sendFunction', mailOptions);
+    // 3) create a transport and send the email
+    await this.newTransporter().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours family!');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)'
+    );
+  }
+};
